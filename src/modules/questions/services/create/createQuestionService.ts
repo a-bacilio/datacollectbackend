8ffe810d5c18b48554bda589260306d08e322d67
Anthony Bacilio Ruiz <bacilio.anthony@gmail.com>
2022-05-11 00:23:11 -0500

    fix

--- src/modules/questions/services/create/createQuestionService.ts
+++ src/modules/questions/services/create/createQuestionService.ts
@@ -1,22 +1,26 @@
-import {Types} from 'mongoose';
-import questionModel from '../../entity/model/questionModel';
-import { ICreateQuestion } from '../../entity/types/questionInterface';
-import IQuestion from '../../entity/types/questionInterface';
-import projectModel from '../../../projects/entity/model/prokectModel';
+import { Types } from "mongoose";
+import questionModel from "../../entity/model/questionModel";
+import { ICreateQuestion } from "../../entity/types/questionInterface";
+import IQuestion from "../../entity/types/questionInterface";
+import projectModel from "../../../projects/entity/model/prokectModel";
 
-
-const createQuestionService = async (question: ICreateQuestion): Promise<IQuestion> => {
+const createQuestionService = async (
+  question: ICreateQuestion
+): Promise<IQuestion> => {
   try {
     if (Object.keys(question).length === 0) {
       throw new Error("question object is empty");
     }
-    const project = await projectModel.findOne({id:question.project});
+    console.log(question);
+    const project = await projectModel.findOne({ _id: question.project });
+    console.log(project);
     const questions = project?.questions;
-    const newQuestion:IQuestion = await questionModel.create({...question,name:question.name.replace(/[^a-zA-Z1-9]/g, "")});
-    questions?.push(
-      newQuestion._id as Types.ObjectId,
-    )
-    await projectModel.findOneAndUpdate({id:question.project},{questions})
+    const newQuestion: IQuestion = await questionModel.create({
+      ...question,
+      name: question.name.replace(/[^a-zA-Z1-9]/g, ""),
+    });
+    questions?.push(newQuestion._id as Types.ObjectId);
+    await project?.save();
 
     return newQuestion;
   } catch (error: any) {
